0a559f7ec294db9aa50c05ac755a49b72b1017f5
Zishengss <186271037+Zishengss@users.noreply.github.com>
2025-04-30 20:36:49 +0100

    Initial commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\86186\Desktop\ap\web\manchester_dashboard\venv
+set VIRTUAL_ENV=C:\Users\jason\Desktop\Apply Data Science\manchester_dashboard_new_newdata\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
